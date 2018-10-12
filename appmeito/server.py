fd485a8b31308b1c8ad9832f3ab97c3ecf3a24f2
nikhil5642 <n_agrawal52@rediffmail.com>
2018-10-12 17:08:21 +0530

    delete database folder

---
+++ appmeito/server.py
@@ -0,0 +1,94 @@
+from flask import Flask,jsonify,request
+import database_info
+import psycopg2
+import insert
+import update
+
+conn = psycopg2.connect(database=database_info.database, user = database_info.user, 
+                    password = database_info.password, host = database_info.host,
+                     port =database_info.port)
+cursor=conn.cursor()
+
+def check_auth(request):
+    try:
+        app_id=request.headers['app_id']
+        app_secret=request.headers['app_secret']
+        cursor.execute("select * from clients where appid='%s' and app_secret='%s'"%(app_id,app_secret))
+        rows=cursor.fetchall()
+        if(len(rows)!=0):
+            return True
+        else:
+            return False        
+    except:
+            return False
+    
+
+app=Flask(__name__)
+@app.route("/test_post",methods=['Post'])
+def event_data():
+    data=request.get_json(force=True)
+    print(data)
+@app.route("/event_data",methods=['POST'])
+def event_data():
+    if(check_auth(request)==False):
+        return jsonify(success=False,message="Invalid Authentication")
+    try: 
+        data=request.get_json(force=True)
+        adid=data['adid']
+        macid=data['macid']
+        appid=data['appid']
+        event_json=data['event_json']
+        for event in event_json:
+            lattitude=event['lattitude']
+            longitude=event['longitude']
+            zipcode=event['code']
+            event_now=event['event']
+            insert.new_event_data(adid,macid,appid,lattitude,longitude,zipcode,event_now)
+        return jsonify(success=True,message=None)
+    except:
+        return jsonify(success=False,message="Invalid body")
+
+@app.route("/user_installation",methods=['POST'])
+def user_installation():
+    if(check_auth(request)==False):
+        return jsonify(success=False,message="Invalid Authentication")
+    try:
+        data=request.get_json(force=True)
+        adid=data['adid']
+        macid=data['macid']
+        appid=data['appid']
+        installation_status='installed'
+        user_category=None
+        activity=data['activity']
+        device_info=data['device_info']
+        cursor.execute("select * from users where adid='%s' and appid='%s'"%(adid,appid))
+        rows=cursor.fetchall()
+        if(len(rows)!=0):
+            update.user_activity(adid,appid,activity)
+        else:
+            insert.new_user(adid,macid,appid,installation_status,user_category,activity,device_info)
+        return jsonify(success=True,message=None)
+    except:
+        return jsonify(success=False,message="Invalid body")
+
+@app.route("/user_uninstallation",methods=['POST'])
+def user_uninstallation():
+    if(check_auth(request)==False):
+        return jsonify(success=False,message="Invalid Authentication")
+    try:
+        data=request.get_json(force=True)
+        adid=data['adid']
+        macid=data['macid']
+        appid=data['appid']
+        installation_status='uninstalled'
+        user_category=None
+        activity=data['activity']
+        device_info=data['device_info']
+        
+        update.user_activity(adid,appid,activity)
+        return jsonify(success=True,message=None)
+    except:
+        return jsonify(success=False,message="Invalid body")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
